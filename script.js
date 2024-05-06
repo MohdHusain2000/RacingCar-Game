a16ef580b31873a2dd8f7542b38f08e5d557db26
Mohamed Husain <mo.abdulla3000@gmail.com>
2024-05-06 04:54:20 -0700

    [2]

---
+++ script.js
@@ -0,0 +1,5 @@
+////////////////////////////////
+// Global Variables Here
+const playerCar = document.getElementById('car')
+let street = document.getElementById('street')
+let opCar = document.getElementById('opCar')
