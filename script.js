ea9f119eb16a1041f23da2c4665b7454335590ee
MohdHusain2000 <167669621+MohdHusain2000@users.noreply.github.com>
2024-05-06 05:19:50 -0700

    9

---
+++ script.js
@@ -0,0 +1,10 @@
+////////////////////////////////
+// Global Variables Here
+const playerCar = document.getElementById('car')
+let street = document.getElementById('street')
+let opCar = document.getElementById('opCar')
+let opCar1 = document.getElementById('opCar1')
+let opCar2 = document.getElementById('opCar2')
+let start = document.getElementById('start')
+let stop = document.getElementById('stop')
+let scoreButton = document.getElementById('score')
