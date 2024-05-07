ac05cdf32484442a431a7595703d38ab7561a3b5
MohdHusain2000 <167669621+MohdHusain2000@users.noreply.github.com>
2024-05-07 15:20:50 -0700

    14
    Event listener was added to move the cars using keyboard arrows.

--- script.js
+++ script.js
@@ -17,3 +17,58 @@ let controlKeys = {
     ArrowRight: false,
     ArrowLeft: false,
 } 
+let playCar= {
+    speed: 0,
+    score: 0,
+    acceleration: 1,
+};
+
+// Function to move the player car
+ const movePlayerCar = () => {
+    document.addEventListener('keydown', (event) => {
+    controlKeys[event.keys = true]
+        if (gameOn) {
+            if (event.key === 'ArrowUp'){
+                Acceleration()
+                moveUp () 
+            } else if (event.key === 'ArrowDown'){
+                moveDown ()
+            } else if (event.key === 'ArrowRight') {
+                moveRight ()
+            } else if (event.key === 'ArrowLeft') {
+                moveLeft()
+            }
+        }
+    })
+    document.addEventListener('keyup', (event) => {
+        controlKeys[event.key] = false;
+    });
+}
+ 
+
+// Function to speed up the car
+const moveUp = () => {
+    let topDir = parseFloat(window.getComputedStyle(playerCar).top);
+    if (topDir+200 >= 0) {
+        playerCar.style.top = (topDir - opcarSpeed) + 'px'
+    } else {
+        playerCar.style.top = 0 +'px'
+    }
+}
+const Acceleration = () =>  {
+    playCar.speed += playCar.acceleration
+    let Dir = parseFloat(playerCar.style.top)
+    if (Dir > 0 || Dir !== null)
+    playerCar.style.top = (Dir-playCar.speed) + 'px'
+}
+
+
+
+
+
+
+
+
+
+
+
