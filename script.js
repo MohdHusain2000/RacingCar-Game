2316bf3f18b57887d51ed8c158c4bbe08322568f
Mohamed Husain <mo.abdulla3000@gmail.com>
2024-05-08 11:09:05 -0700

    Racing Car project

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let street = document.querySelector('.streets')
 let opCar = document.getElementById('opCar')
 let opCar1 = document.getElementById('opCar1')
 let opCar2 = document.getElementById('opCar2')
+let opCar3 = document.getElementById('opCar3')
 let start = document.getElementById('start')
 let stop = document.getElementById('stop')
 let scoreButton = document.getElementById('score')
@@ -37,17 +38,14 @@ let playCar= {
     playCar.score
     scoreResult()
     movePlayerCar()
-    animate()
-    animate1()
     highScore()
-     
+    gameLogic()
 }
 
 // Function to stop the game
  const stopPlay = () => {
     gameOn = false;
     stopAnimate()
-    stopanimate1()
 }
 
 // Function to count the Car.score
@@ -58,10 +56,10 @@ let playCar= {
 // Function to move the player car
  const movePlayerCar = () => {
     document.addEventListener('keydown', (event) => {
-    controlKeys[event.keys == true]
+    controlKeys[event.keys = true]
         if (gameOn) {
             if (event.key === 'ArrowUp'){
-                Acceleration()
+                accelerations()
                 moveUp () 
             } else if (event.key === 'ArrowDown'){
                 moveDown ()
@@ -87,7 +85,7 @@ const moveUp = () => {
         playerCar.style.top = 0 +'px'
     }
 }
-const Acceleration = () =>  {
+const accelerations = () =>  {
     playCar.speed += playCar.acceleration
     let Dir = parseFloat(playerCar.style.top)
     if (Dir > 0 || Dir !== null)
@@ -97,7 +95,7 @@ const Acceleration = () =>  {
 // Function to maintain the cars speed
 const moveDown = () => {
     let topDir = parseFloat(window.getComputedStyle(playerCar).top)
-    let maxHeight = 90 - playerCar.offsetHeight
+    let maxHeight = 80 - playerCar.offsetHeight
     if (topDir < 0) {
         playerCar.style.top = (maxHeight+opcarSpeed) +'px'
     }
@@ -114,7 +112,7 @@ const moveLeft = () => {
  // Function to move the player car right
 const moveRight = () => {
     let leftDir = parseFloat(window.getComputedStyle(playerCar).left);
-    let width = 150 - playerCar.offsetWidth;
+    let width = 130 - playerCar.offsetWidth;
     if (leftDir < width) {
         playerCar.style.left = (leftDir + opcarSpeed) + 'px'
     }
@@ -124,7 +122,7 @@ const moveRight = () => {
  const highScore = () => {
     let interval = setInterval(() => {
         if (gameOn) {
-            playCar.score += 1
+            playCar.score += 3
             scoreResult();
         } else {
             clearInterval(interval);
@@ -133,46 +131,160 @@ const moveRight = () => {
 }
 
 // Function to animate opponents cars
-const animate = () => {
+const animate1 = () => {
     opCar.classList.add('animateop')
     opCar1.classList.add('animateop1')
     opCar2.classList.add('animateop2')
+    opCar3.style.display= 'none'
     street.classList.add('animatestr') 
 }
 
-const animate1 = () => {
-    opCar.classList.add('animateop01').
+// Function to animate opponents cars
+const animate2 = () => {
+    opCar.classList.add('animateop01')
     opCar1.classList.add('animateop02')
     opCar2.classList.add('animateop03')
-    street.classList.add('animatestr1') 
+    opCar3.style.display= 'none'
+    street.classList.add('animatestr1')
+
+    if (playCar.score >= 800 && playCar.score <1000){
+        opCar.style.marginLeft='100vh';
+        opCar1.style.marginLeft='80vh';
+        opCar3.style.display= 'none'
+    }
 }
 
-// Function to move a car element
-const stopAnimate1 = () => {
+const animate3 = () => {
+        opCar.classList.add('animateop001')
+        opCar1.classList.add('animateop002')
+        opCar2.classList.add('animateop003')
+        opCar3.classList.add('animateop0004')
+        street.classList.add('animatestr2')
+        
+        if (playCar.score >= 1000 && playCar.score<1250){
+            opCar.style.marginLeft='83vh';
+            opCar1.style.marginLeft='110vh';
+            opCar3.style.display= 'none'
+        }
+
+        if (playCar.score >= 1250 && playCar.score<1500){
+            opCar.style.marginLeft='80vh';
+            opCar1.style.marginLeft='105vh';
+}
+}
+
+const animate4 = () => {
+    opCar.classList.add('animateop0001')
+    opCar1.classList.add('animateop0002')
+    opCar2.classList.add('animateop0003')
+    opCar3.classList.add('animateop0004')
+    street.classList.add('animatestr2')
+    opCar3.style.display= 'block'
+
+}
+
+ // Function to move a car element
+ const stopAnimate = () => {
+    //* Less than 500
     opCar.classList.remove('animateop')
     opCar1.classList.remove('animateop1')
     opCar2.classList.remove('animateop2')
     street.classList.remove('animatestr')
-
-}
-
-const stopAnimate = () => {
+ 
+    //* More than 500
     opCar.classList.remove('animateop01')
     opCar1.classList.remove('animateop02')
     opCar2.classList.remove('animateop03')
     street.classList.remove('animatestr1')
+    
+    //* More than 1000
+    opCar.classList.remove('animateop001')
+    opCar1.classList.remove('animateop002')
+    opCar2.classList.remove('animateop003')
+    street.classList.remove('animatestr1')
 
+    //* More than 1200
+    opCar.classList.remove('animateop0001')
+    opCar1.classList.remove('animateop0002')
+    opCar2.classList.remove('animateop0003')
+    opCar3.classList.remove('animateop0004')
+    street.classList.remove('animatestr2')
+    
 }
 
-// Function for collision
+ // Function for collision
+ const findCollision = () => {
+    let playerRect = playerCar.getBoundingClientRect();
+    let opCarRect = opCar.getBoundingClientRect();
+    let opCarRect1 = opCar1.getBoundingClientRect();
+    let opCarRect2 = opCar2.getBoundingClientRect();
+    let opCarRect3 = opCar3.getBoundingClientRect();
+    
+ //* Detect collisions
+    let topBoundary = playerRect.top + playerCar.offsetHeight * 0.1
+    let bottomBoundary = playerRect.bottom - playerCar.offsetHeight * 0.1;
 
+ if (topBoundary < opCarRect.bottom && bottomBoundary > opCarRect.top &&
+    playerRect.left < opCarRect.right && playerRect.right > opCarRect.left) {
+    gameOff()
+ }  else if (topBoundary < opCarRect1.bottom && bottomBoundary > opCarRect1.top &&
+    playerRect.left < opCarRect1.right && playerRect.right > opCarRect1.left){
+    gameOff()
+    }else if (topBoundary < opCarRect2.bottom && bottomBoundary > opCarRect2.top &&
+    playerRect.left < opCarRect2.right && playerRect.right > opCarRect2.left){
+    gameOff()
+    }else if (topBoundary < opCarRect3.bottom && bottomBoundary > opCarRect3.top &&
+    playerRect.left < opCarRect3.right && playerRect.right > opCarRect3.left){
+    gameOff()
+    }
+ }
+ 
+ // Function for game over
+ const gameOff = () => {
+    stopPlay()
+    let message = document.querySelector('.content')
+    message.innerText = 'Game over! your score is: ' + playCar.score
 
+ }
+ const gameLogic = () => {
+    if (gameOn == true) {
+        findCollision();
+        gameLevel();
+        requestAnimationFrame(gameLogic);
+    }
+    else {
+        
+    }
+}
 
-// Function to get the winner
+ // Function to get the winner
+ const gameOutcome = () =>{
+    gameOn= false;
+    const highScore = localStorage.getItem("highScore");
+    if (player.score > highScore) {
+      localStorage.setItem("highScore", play.score);
+      score.innerText = `New High Score! Score: ${player.score}`;
+    } else {
+      score.innerText = `Game Over Score was ${player.score}`;
+    }
+  }
+
+// function to increase game hardness
+ const gameLevel = () => {
+     if (gameOn == true) {
+        if (playCar.score <500){
+           animate1()
+        } else if (playCar.score >=600 && playCar.score <1000){
+           animate2()
+        } else if (playCar.score >=1000 && playCar.score <1500){
+           animate3()
+        } else if (playCar.score >=1500){
+           animate4()
+     } 
+ }}
 
 ////////////////////////////////
 // Rendering Function
-
 start.addEventListener('click', startPlay)
 stop.addEventListener('click', stopPlay)
 
